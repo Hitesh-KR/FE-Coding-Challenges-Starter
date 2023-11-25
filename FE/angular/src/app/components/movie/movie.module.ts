aede262374597a07b78976e52934c523fd3d0a33
Hitesh K R <hitesh2695@gmail.com>
2023-11-25 15:55:27 +0530

    refactor: create movie details component

--- FE/angular/src/app/components/movie/movie.module.ts
+++ FE/angular/src/app/components/movie/movie.module.ts
@@ -4,9 +4,10 @@ import { NavigationModule } from '../navigation/navigation.module';
 import { CommonModule } from '@angular/common';
 import { MovieComponent } from './movie.component';
 import { SharedModule } from '../shared/shared.module';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [MovieComponent],
+  declarations: [MovieComponent, DetailsComponent],
   imports: [MovieRoutingModule, NavigationModule, CommonModule, SharedModule]
 })
 export class MovieModule {}
